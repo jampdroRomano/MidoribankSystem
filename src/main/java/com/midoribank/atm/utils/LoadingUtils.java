4b329488bd56bc68246c3d35c9892debc6ed7767
Joao Pedro Romano <jpvs06x0805@gmail.com>
2025-11-14 13:59:13 -0300

    fix: Adjust loading node dimensions and binding in LoadingUtils

--- src/main/java/com/midoribank/atm/utils/LoadingUtils.java
+++ src/main/java/com/midoribank/atm/utils/LoadingUtils.java
@@ -1,10 +1,12 @@
 package com.midoribank.atm.utils;
 
+import com.midoribank.atm.App;
+import com.midoribank.atm.controllers.LoadingController;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import com.midoribank.atm.App;
-import com.midoribank.atm.controllers.LoadingController;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -19,6 +21,13 @@ public class LoadingUtils {
             loadingNode = loader.load();
             controller = loader.getController();
             loadingNode.setMouseTransparent(false);
+
+            if (loadingNode instanceof Pane && !((Pane) loadingNode).getChildren().isEmpty() && ((Pane) loadingNode).getChildren().get(0) instanceof Pane) {
+                Pane innerPane = (Pane) ((Pane) loadingNode).getChildren().get(0);
+                innerPane.layoutXProperty().bind(((Pane) loadingNode).widthProperty().subtract(innerPane.getPrefWidth()).divide(2));
+                innerPane.layoutYProperty().bind(((Pane) loadingNode).heightProperty().subtract(innerPane.getPrefHeight()).divide(2));
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
             loadingNode = null;
@@ -34,6 +43,8 @@ public class LoadingUtils {
                 }
 
                 if (!App.getRootPane().getChildren().contains(loadingNode)) {
+                    ((Region)loadingNode).prefWidthProperty().bind(App.getRootPane().widthProperty());
+                    ((Region)loadingNode).prefHeightProperty().bind(App.getRootPane().heightProperty());
                     App.getRootPane().getChildren().add(loadingNode);
                 }
             } else {
@@ -45,6 +56,8 @@ public class LoadingUtils {
     public static void hideLoading() {
         Platform.runLater(() -> {
             if (loadingNode != null) {
+                ((Region)loadingNode).prefWidthProperty().unbind();
+                ((Region)loadingNode).prefHeightProperty().unbind();
                 App.getRootPane().getChildren().remove(loadingNode);
             }
         });
